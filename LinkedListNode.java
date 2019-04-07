152f3ad66c6d19a6ccac3c526b3d3767eb2a1443
AfnanMousa <48152262+AfnanMousa@users.noreply.github.com>
2019-04-07 16:51:36 +0200

    Add files via upload

---
+++ LinkedListNode.java
@@ -0,0 +1,20 @@
+
+public class LinkedListNode {
+    int value;
+    LinkedListNode next_node;
+   /* public LinkedListNode(int x,LinkedListNode y)
+    {
+    	this. value=x;
+    	this.next_node=y;
+    }*/
+    
+   public int getValue()
+   {
+	   return value;
+   }
+   public LinkedListNode getNext()
+   {
+	   return next_node;
+   }
+  
+}
